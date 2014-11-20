8f3588d0b77a1c8add516b519dcdcd3a9b750d4d
Owen Evans <owen@bgeek.net>
2014-11-21 00:09:10 +1300

    move to sqs broker

--- tests/unit_tests/application_event_host_tests.js
+++ tests/unit_tests/application_event_host_tests.js
@@ -2,17 +2,21 @@
 require('../bootstrap');
 var sinon = require('sinon');
 var mongoose = require('hoist-model')._mongoose;
-var EventBroker = require('broker/lib/event_broker');
+var EventBroker = require('broker');
 var expect = require('chai').expect;
 describe('application_event_host', function () {
   before(function () {
     sinon.stub(mongoose, 'connect').callsArg(1);
-    sinon.stub(EventBroker, 'subscribe').callsArg(1);
+    sinon.stub(EventBroker.prototype, 'listen').callsArg(1);
     require('../../lib/application_event_host');
   });
   it('subscribes to ApplicationEvents', function () {
-    expect(EventBroker.subscribe)
-      .to.be.calledWith(require('broker/lib/event_types/application_event'));
+    expect(EventBroker.prototype.listen)
+      .to.be.calledWith(EventBroker.events.ApplicationEvent);
+  });
+  it('sets model for broker', function () {
+    expect(EventBroker.ModelResolver.get())
+      .to.eql(require('hoist-model'));
   });
   it('opens mongo connection', function () {
     expect(mongoose.connect)
